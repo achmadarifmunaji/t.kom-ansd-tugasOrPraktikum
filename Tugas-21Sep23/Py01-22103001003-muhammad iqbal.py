eaefdc50594a7b0a839c69c804d63b522c7b9c6f
MUHAMMAD IQBAL <113097692+Muhammadiqbal0@users.noreply.github.com>
2023-09-27 01:32:52 +0700

    ijin pak :)

---
+++ Tugas-21Sep23/Py01-22103001003-muhammad iqbal.py
@@ -0,0 +1,28 @@
+#variabel
+a = 5
+b = 3
+c = 2
+d = 9
+
+#penjumlahan
+hasil = a + b + c + d
+print(hasil)
+#pengurangan
+hasil = a - b - c - d
+print(hasil)
+#prkalian
+hasil = a * b * c * d
+print(hasil)
+
+#if dan else
+nama = "iqbal"
+if nama == "amatnya ivi":
+    print("asli ganteng nya ga tertolong !!!")
+else:
+    print("kamu jelek banget sadar diri lah !!!")
+
+#perulangan for loop
+for x in range(10):
+    print("iqbal diakui dunia sanagat ganteng....!!!")
+
+print("iri bilang boss ><")
