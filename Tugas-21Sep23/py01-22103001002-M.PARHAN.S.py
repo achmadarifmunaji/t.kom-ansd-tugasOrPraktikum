0dc46e039ec6055a5f5912256e512ec4c4d3e23d
farhan susilo <113097778+mhmmdfarhan023@users.noreply.github.com>
2023-09-26 22:21:20 +0700

    ijin ngumpul tugas

---
+++ Tugas-21Sep23/py01-22103001002-M.PARHAN.S.py
@@ -0,0 +1,32 @@
+#1
+print("hello world")
+
+#2
+x = 5 # Variabel x dengan nilai 5
+y = 3 # Variabel y dengan nilai 3
+nama = "john" # Variabel nama dengan nilai "john"
+print(x + y) # hasilnya adalah 8
+print(x - y) # hasilnya adalah 2
+print(x * y) # hasilnya adalah 15
+
+#3
+BB = 80 
+
+if BB >= 80:
+    print("Udin gemuk")
+else :
+    print("Udin kurus")    
+
+#4
+#for loop 
+fruits = ["semangaka", "durian", "nangka"]
+for fruit in fruits:
+    print(fruit)
+
+#while loop
+count = 0
+while count < 8:
+    print(count)
+    count += 1    
+
+#TERIMA KASIH#    
