b6112915d74c81fb5e7ec333035878460e06b7f9
yusronnashrullohh <113098188+yusronnashrullohh@users.noreply.github.com>
2023-09-27 16:15:59 +0700

    izin kirim tugas pak

---
+++ Tugas-21Sep23/Py01-22103001005-Muhammad Yusron Nashrulloh.py
@@ -0,0 +1,24 @@
+# Nomor 1: Cetak "Hello, World!"
+print("Hello, World!")
+
+# Nomor 2: Variabel dan Operasi Matematika
+a = 5
+b = 3
+c = 7
+
+hasil_penjumlahan = a + b
+hasil_pengurangan = a - c
+hasil_perkalian = b * c
+
+# Nomor 3: Struktur Pengkondisian (if-else)
+umur = 20
+
+if umur >= 18:
+    print("Anda adalah seorang dewasa.")
+else:
+    print("Anda masih di bawah umur.")
+
+# Nomor 4: Looping Perulangan
+for i in range(5):
+    print("Perulangan ke-", i)
+
