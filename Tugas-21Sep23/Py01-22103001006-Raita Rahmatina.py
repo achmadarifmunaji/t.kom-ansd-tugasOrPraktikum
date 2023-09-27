3beb30d9f1d851f2afe238123eb05085e3d33ff8
Raita22 <113097574+Raita22@users.noreply.github.com>
2023-09-27 20:40:44 +0700

    izin ngumpul tugas

---
+++ Tugas-21Sep23/Py01-22103001006-Raita Rahmatina.py
@@ -0,0 +1,17 @@
+print ('Hello world')
+Yupi = 5
+Kopiko = 7
+Relaxa = 3
+
+print (Yupi + Relaxa)
+print (Kopiko * Yupi)
+print (Relaxa - Kopiko)
+
+if Yupi > Relaxa:
+    print ("benar")
+else:
+     print ("salah")
+
+Permen = ["Yupi", "Kopiko", "Relaxa"]
+for Permen in Permen :
+     print (Permen)
