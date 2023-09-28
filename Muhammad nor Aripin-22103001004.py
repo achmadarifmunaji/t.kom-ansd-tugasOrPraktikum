6c94b02f1f99eefee3dfdacc5e223c155b29fe44
ifin498 <136960882+ifin498@users.noreply.github.com>
2023-09-28 22:34:28 +0600

    ngumpul tugas

---
+++ Muhammad nor Aripin-22103001004.py
@@ -0,0 +1,15 @@
+print("Hello world")
+Mangga = 8000
+Jeruk = 5000
+Sawo = 3000
+print("=", Mangga + Jeruk)
+print("=", Jeruk - Sawo)
+print("=", Mangga * Sawo)
+if Mangga > Jeruk :
+    print("Dijual")
+else :
+    print("Diberi")
+Buah =0
+while Buah < 6 :
+    print(Buah)
+    Buah += 1
